74a55a1c3531d521d7809e08d14c7ca92b2f59cd
Shadin Kappachali <iedc25to26@gmail.com>
2026-02-06 18:24:57 +0530

    Chore: Removed Flash Sale and Deals of the Week sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { Hero } from "@/components/home/hero";
 import { CategoriesGrid } from "@/components/home/categories-grid";
 import { BestSellers } from "@/components/home/best-sellers";
 import { NewArrivals } from "@/components/home/new-arrivals";
-import { DealsCountdown } from "@/components/home/deals-countdown";
 import { PCBuilderPromo } from "@/components/home/pc-builder-promo";
 import { TrustBadges } from "@/components/home/trust-badges";
 import { VisitStore } from "@/components/home/visit-store";
@@ -15,7 +14,6 @@ export default function Home() {
       <BestSellers />
       <NewArrivals />
       <PCBuilderPromo />
-      <DealsCountdown />
       <VisitStore />
       <TrustBadges />
     </div>
